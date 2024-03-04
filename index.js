45983c94c6388930889bbfb8ae29cd0e8c6a2777
ezekielswanson <swansonezekiel@gmail.com>
2024-03-03 20:54:02 -0600

    Updated js

---
+++ index.js
@@ -0,0 +1,10 @@
+const apiKey = AIzaSyCEUkGWb7OAUjT6ZWGgac9o-_QzpGROy2U;
+//const endpoint = "https://www.googleapis.com/youtube/v3/search"
+const channelId = UCHa8J-xnRYOg5VuudfWpBgg;
+
+
+async function fetchVideos(inputValue) {
+    try {
+        const res = await fetch(`https://www.googleapis.com/youtube/v3/search`)
+    }
+}
