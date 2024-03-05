9ef1614e29661e5deb487182f07c37ccf36594b5
ezekielswanson <swansonezekiel@gmail.com>
2024-03-04 21:15:19 -0600

    Updated

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const apiKey = AIzaSyCEUkGWb7OAUjT6ZWGgac9o-_QzpGROy2U;
-//const endpoint = "https://www.googleapis.com/youtube/v3/search"
-const channelId = UCHa8J-xnRYOg5VuudfWpBgg;
+const apiKey = 'AIzaSyCEUkGWb7OAUjT6ZWGgac9o-_QzpGROy2U';
+const channelId = 'UCHa8J-xnRYOg5VuudfWpBgg';
 
 
-async function fetchVideos(inputValue) {
+//Fetch videos by search
+async function fetchVideos(videoTitle) {
     try {
-        const res = await fetch(`https://www.googleapis.com/youtube/v3/search`)
+        const res = await fetch(`https://www.googleapis.com/youtube/v3/search?apikey=${apiKey}&channelId=${channelId}&type=video&maxResults=6&q=${encodeURIComponent(videoTitle)}`);
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+    
+        const data = await res.json()
+        console.log(data)
     }
+
+    catch (error) {
+        console.error('No videos match your search - Please search another term.:', error);
+    }
+    
 }
+
+//Click event
+document.querySelector('.search-form__button').addEventListener('click', function() {
+    const videoTitle = document.querySelector('.search-form__input').value;
+    if (videoTitle) {
+        fetchVideos(videoTitle)
+    }
+
+})
