e83582cc029138705a15e34a1364be317c1b467f
ezekielswanson <swansonezekiel@gmail.com>
2024-03-14 21:15:43 -0500

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const apiKey = 'AIzaSyCEUkGWb7OAUjT6ZWGgac9o-_QzpGROy2U';
 const channelId = 'UCHa8J-xnRYOg5VuudfWpBgg';
 
-// Fetch videos by search
-async function fetchVideos(videoTitle) {
+
+/*
+//Render Channel On Load
+document.addEventListener('load', function() {
+    //invoke my function here
+
+})
+
+
+
+async function showChannelOnLoad() {
     try {
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&type=video&maxResults=6&part=snippet&q=${encodeURIComponent(videoTitle)}`);
+        const res = await fetch(`https://www.googleapis.com/youtube/v3/`)
+    }
+
+}
+*/
+
+
+
+// Click event
+document.querySelector('.search-form__button').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const videoTitle = document.querySelector('.search-form__input').value;
+    if (videoTitle) {
+        fetchVideosBySearch(videoTitle);
+
+        //more explicit
+    }
+});
+
+
+// Fetch videos by search
+async function fetchVideosBySearch(videoQuery) {
+    try {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&type=video&maxResults=6&part=snippet&q=${encodeURIComponent(videoQuery)}`);
         
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
@@ -13,16 +46,7 @@ async function fetchVideos(videoTitle) {
         const data = await response.json();
         console.log(data);
     } catch (error) {
-        console.error('No videos match your search - Please search another term:', error);
+        console.error('No videos match your search - Please search another video name:', error);
     }   
 }
 
-// Click event
-document.querySelector('.search-form__button').addEventListener('click', function(e) {
-    e.preventDefault();
-
-    const videoTitle = document.querySelector('.search-form__input').value;
-    if (videoTitle) {
-        fetchVideos(videoTitle);
-    }
-});
